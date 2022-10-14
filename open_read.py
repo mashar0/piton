988e42a511cab0bdd08bed182b5dfa4618a7cd84
AdiPrm <adipr671@gmail.com>
2022-10-15 02:11:38 +0700

    nyoba

--- open_read.py
+++ open_read.py
@@ -2,6 +2,13 @@ import sys
 from sys import platform
 from time import sleep 
 import os
+
+def clear():
+    if platform == 'linux' or platform == 'linux2':
+        os.system('clear')
+    elif platform == 'win32':
+        os.system('cls')
+
 try:
     from tkinter import *
     from tkinter import filedialog
@@ -16,7 +23,7 @@ lokasi_file = filedialog.askopenfilename(title="Open File Yang Akan Di Baca")
 
 with open(lokasi_file, mode="r") as file:
     isi_data = file.read()
-    os.system('clear')
+    clear()
     print("Isi Data Nya adalah : \n")
     for i in isi_data:
         sys.stdout.write(i)
